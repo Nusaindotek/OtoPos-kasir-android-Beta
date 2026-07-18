23339512f20186e04b4d1d294ba0b25a17cb6f39
Nusaindotek <nusaindoteknologi@gmail.com>
2026-07-18 18:24:55 +0700

    Update app.js

--- app.js
+++ app.js
@@ -22,5 +22,42 @@ function openTab(id, event) {
     activeBtn.classList.remove('border-transparent', 'text-gray-500');
     activeBtn.classList.add('border-blue-600', 'text-blue-600');
 }
+function simpanSettingToko() {
+    // Mengambil elemen dengan aman
+    const elNama = document.getElementById('setting-nama');
+    const elAlamat = document.getElementById('setting-alamat');
+    const elTelp = document.getElementById('setting-telp');
+    const elFooter = document.getElementById('setting-footer');
+
+    // Update object setting
+    if(elNama) setting.nama = elNama.value;
+    if(elAlamat) setting.alamat = elAlamat.value;
+    if(elTelp) setting.telp = elTelp.value;
+    if(elFooter) setting.footer = elFooter.value;
+
+    // Simpan ke LocalStorage
+    localStorage.setItem('otopos_setting_toko', JSON.stringify(setting));
+    
+    // Update tampilan header secara langsung
+    const headerNama = document.getElementById('nama-toko-header');
+    if(headerNama) headerNama.innerText = setting.nama;
+
+    alert('Data Toko Berhasil Disimpan!');
+}
+
+function loadSetting() { 
+    const elNama = document.getElementById('setting-nama');
+    const elAlamat = document.getElementById('setting-alamat');
+    const elTelp = document.getElementById('setting-telp');
+    const elFooter = document.getElementById('setting-footer');
+    
+    if(elNama) elNama.value = setting.nama;
+    if(elAlamat) elAlamat.value = setting.alamat;
+    if(elTelp) elTelp.value = setting.telp;
+    if(elFooter) elFooter.value = setting.footer;
+    
+    const headerNama = document.getElementById('nama-toko-header');
+    if(headerNama) headerNama.innerText = setting.nama;
+}
 
 // ... (sisanya isi dengan fungsi-fungsi lain yang sudah ada)
