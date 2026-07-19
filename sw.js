e6aecc8169d2e6eb9a53027e1754a32d5dfe0906
Nusaindotek <nusaindoteknologi@gmail.com>
2026-07-19 18:14:05 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,48 @@
-const CACHE_NAME = 'otopos-v1';
-const ASSETS = ['index.html', 'style.css', 'app.js', 'gambarotopos192.png', 'gambarotopos512.png'];
+const CACHE_NAME = 'otopos-v1.5';
+const urlsToCache = [
+  './',
+  'index.html',
+  'style.css',
+  'app.js',
+  'manifest.json',
+  'gambarotopos192.png',
+  'gambarotopos512.png'
+];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+// Install - simpan semua file ke cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache dibuka');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+// Fetch - kalau offline ambil dari cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Kalau ada di cache, pakai cache. Kalau nggak ada, ambil dari internet
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Update cache kalau ada versi baru
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
